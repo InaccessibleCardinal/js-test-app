266ffd2e30e0b5d49924eb96267ffa780a67ab05
InaccessibleCardinal <kennethlandsbaum@hotmail.com>
2019-01-27 11:33:22 -0600

    experiment: little virtual dom to play with

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import {question1} from './question1';
-import {question2} from './question2';
-import {question3} from './question3';
-import {question4} from './question4';
-import {question5} from './question5';
-import {question6} from './question6';
-import {question9} from './question9';
+// import {question1} from './question1';
+// import {question2} from './question2';
+// import {question3} from './question3';
+// import {question4} from './question4';
+// import {question5} from './question5';
+// import {question6} from './question6';
+// import {question9} from './question9';
+import application from './application/';
 
 if (typeof window !== 'undefined') {
     window.addEventListener('DOMContentLoaded', app);
     function app() {
         console.log('app is working...');
-        question1();
-        question2();
-        question3();
-        question4();
-        question5();
-        question9();
+        // question1();
+        // question2();
+        // question3();
+        // question4();
+        // question5();
+        //question9();
+        application();
     }
 }
 
